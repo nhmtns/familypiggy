1a928c7ca7b60c9629b35fa2c6d557f81da6fd2f
srini vanga <nhmtns@yahoo.com>
2018-01-20 09:20:27 -0500

    Initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Family Piggy
+__A powerful finances and investments management platform for whole family__
+
+
+This is a Microservices architecture learning project built with _Spring Boot, Spring Cloud, H2 Database and Docker_.  
+
---
+++ src/main/java/com/srini/piggy/FamilypiggyApplication.java
@@ -0,0 +1,12 @@
+package com.srini.piggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilypiggyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilypiggyApplication.class, args);
+	}
+}
